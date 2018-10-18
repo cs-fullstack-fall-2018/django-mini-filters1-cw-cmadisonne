a267ebf34d016a57130a2794ab69faae3d4ac6ed
Lotus <cmadisonne@gmail.com>
2018-10-18 15:02:09 -0500

    Complete

--- filter_project/filter_project/settings.py
+++ filter_project/filter_project/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'filter_app1',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'filter_app1',
 ]
 
 MIDDLEWARE = [
